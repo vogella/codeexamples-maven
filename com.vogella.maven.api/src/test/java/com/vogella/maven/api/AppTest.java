4a75e2b6879369a376601e7e0b2b6d57a17314bd
Lars Vogel <Lars.Vogel@vogella.com>
2021-01-31 14:52:03 +0100

    Initial commit

---
+++ com.vogella.maven.api/src/test/java/com/vogella/maven/api/AppTest.java
@@ -0,0 +1,21 @@
+package com.vogella.maven.api;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnIswerWithTrue()
+    {
+    	App app = new App();
+        assertTrue( true );
+    }
+}
